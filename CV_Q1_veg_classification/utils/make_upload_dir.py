fb0d1c36f34ed9419ff931770c637425c4698f32
mldl007 <104562700+mldl007@users.noreply.github.com>
2023-05-22 14:50:17 +0530

    Delete CV_Q1_veg_classification directory

--- CV_Q1_veg_classification/utils/make_upload_dir.py
+++
@@ -1,8 +0,0 @@
-from os import path, makedirs
-from shutil import rmtree
-
-
-def make_upload_dir(path_: str):
-    if path.exists(path_):
-        rmtree(path=path_, ignore_errors=True)
-    makedirs(path_)
